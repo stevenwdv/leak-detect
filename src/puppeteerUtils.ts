253d7a8f3adc30934ca74de7e1af1aeb01b8894e
StevenWdV <stevenwdv@gmail.com>
2022-06-07 23:33:45 +0200

    Improve `evaluate*` wrappers

--- src/puppeteerUtils.ts
+++ src/puppeteerUtils.ts
@@ -1,4 +1,13 @@
-import {ElementHandle, Frame, JSHandle, Page, Serializable, SerializableOrJSHandle} from 'puppeteer';
+import {
+	DOMWorld,
+	ElementHandle,
+	ExecutionContext,
+	Frame,
+	JSHandle,
+	Page,
+	Serializable,
+	SerializableOrJSHandle,
+} from 'puppeteer';
 import TypedArray = NodeJS.TypedArray;
 
 // puppeteer does not actually export its classes, so we cannot use instanceof and instead need this stupid stuff
@@ -28,18 +37,22 @@ export function getFrameStack(frame: Frame): Frame[] {
 	return frames;
 }
 
-/** Typed version of {@link Frame#evaluateHandle} and {@link JSHandle#evaluateHandle} */
-export async function evaluateHandle<Target extends Frame | JSHandle, Args extends SerializableOrJSHandle[], Return>(
-	  target: Target, pageFunction: PageFunction<Target, Args, Return>, ...args: Args): Promise<JSHandle<Return>> {
-	return await target.evaluateHandle(pageFunction, ...args);
+/** Typed version of {@link Frame#evaluateHandle}, {@link JSHandle#evaluateHandle}, etc. */
+export async function evaluateHandle<Subject extends Frame | Page | ExecutionContext | DOMWorld | JSHandle,
+	  Args extends SerializableOrJSHandle[], Return>(
+	  subject: Subject, pageFunction: PageFunction<Subject, Args, Return>, ...args: Args): Promise<JSHandle<Return>> {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore This compiles for all Targets individually
+	return await subject.evaluateHandle(pageFunction, ...args) as JSHandle<Return>;
 }
 
-/** Typed version of {@link Frame#evaluate} and {@link JSHandle#evaluate} */
-export async function evaluate<Target extends Frame | JSHandle, Args extends SerializableOrJSHandle[], Return extends Serializable | void>(
-	  target: Target, pageFunction: PageFunction<Target, Args, Return>, ...args: Args): Promise<Awaited<Return>> {
+/** Typed version of {@link Frame#evaluate}, {@link JSHandle#evaluate}, etc. */
+export async function evaluate<Subject extends Frame | Page | ExecutionContext | DOMWorld | JSHandle,
+	  Args extends SerializableOrJSHandle[], Return extends Serializable | PromiseLike<Serializable> | void>(
+	  subject: Subject, pageFunction: PageFunction<Subject, Args, Return>, ...args: Args): Promise<Awaited<Return>> {
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore This compiles for both Frame & JSHandle individually
-	return await target.evaluate(pageFunction as never, ...args) as Awaited<Return>;
+	// @ts-ignore This compiles for all Targets individually
+	return await subject.evaluate(pageFunction as never, ...args) as Awaited<Return>;
 }
 
 // Need the ElementHandle case because inferring U on JSHandle gives unknown because currently JSHandle<A> extends JSHandle<B> for any A,B...
@@ -47,14 +60,10 @@ type HandleValue<H extends JSHandle> = H extends ElementHandle<infer U> ? U
 	  : H extends JSHandle<infer U> ? U : never;
 
 type Passed<H extends SerializableOrJSHandle> = H extends JSHandle ? HandleValue<H> : H;
-type PassedArgs<Args extends SerializableOrJSHandle[]> =
-	  Args extends [] ? []
-			: Args extends [infer T0 extends SerializableOrJSHandle, ...infer Ts extends SerializableOrJSHandle[]]
-				  ? [Passed<T0>, ...PassedArgs<Ts>]
-				  : Args extends (infer T extends SerializableOrJSHandle)[]
-						? Passed<T>[] : unknown;
+type PassedArgs<Args extends SerializableOrJSHandle[]> = { [K in keyof Args]: Passed<Args[K]> };
 
-type PageFunction<Target extends Frame | JSHandle, Args extends SerializableOrJSHandle[], Return> =
+type PageFunction<Target extends Frame | Page | ExecutionContext | DOMWorld | JSHandle,
+	  Args extends SerializableOrJSHandle[], Return> =
 	  Target extends JSHandle
 			? (obj: HandleValue<Target>, ...args: PassedArgs<Args>) => Return
 			: (...args: PassedArgs<Args>) => Return;
