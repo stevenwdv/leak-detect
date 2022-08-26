2558dd7e5aa40f42d89c294d9f3af1263fd6d0ab
StevenWdV <stevenwdv@gmail.com>
2022-08-26 12:45:26 +0200

    Update findValue tests

--- test/src/analysis.spec.ts
+++ test/src/analysis.spec.ts
@@ -13,14 +13,33 @@ void t.test(findValue.name, async t => {
 				method: 'GET',
 				type: 'Document',
 				remoteIPAddress: '0.0.0.0',
-				requestHeaders: [],
-				responseHeaders: [],
+				requestHeaders: {},
+				responseHeaders: {},
 				failureReason: '',
 			};
 			t.strictSame(await findValue(searcher, [request]), [{
-				request,
+				requestIndex: 0,
 				part: 'url',
 				encodings: ['uri'],
+			}]);
+		}),
+		t.test('searches in request headers', async t => {
+			const request: RequestCollector.RequestData = {
+				url: 'https://example.com/',
+				method: 'GET',
+				type: 'Document',
+				remoteIPAddress: '0.0.0.0',
+				requestHeaders: {
+					Cookie: 'hello=The--P@s5w0rd',
+				},
+				responseHeaders: {},
+				failureReason: '',
+			};
+			t.strictSame(await findValue(searcher, [request]), [{
+				requestIndex: 0,
+				part: 'header',
+				header: 'Cookie',
+				encodings: [],
 			}]);
 		}),
 		t.test('searches in body', async t => {
@@ -30,15 +49,24 @@ void t.test(findValue.name, async t => {
 				type: 'Document',
 				postData: 'email=leak-detector%40example.com&password=The--P%40s5w0rd',
 				remoteIPAddress: '0.0.0.0',
-				requestHeaders: [],
-				responseHeaders: [],
+				requestHeaders: {},
+				responseHeaders: {},
 				failureReason: '',
 			};
 			t.strictSame(await findValue(searcher, [request]), [{
-				request,
+				requestIndex: 0,
 				part: 'body',
 				encodings: ['uri'],
 			}]);
 		}),
+
+		t.test('searches in visited targets', async t =>
+			  t.strictSame(await findValue(searcher, [],
+					['https://example.com/?email=leak-detector%40example.com&password=The--P%40s5w0rd'],
+			  ), [{
+				  visitedTargetIndex: 0,
+				  part: 'url',
+				  encodings: ['uri'],
+			  }])),
 	]);
 });
